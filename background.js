cb4cc4423855c9f7bc4cde2ed2287b1a5a494a3e
nicolas-godefroy-dev <nicolas.godefroy.dev@gmail.com>
2025-01-22 12:01:59 +0100

    feat: compact ui

--- background.js
+++ background.js
@@ -1,209 +1,211 @@
 var trackedEvents = new Array();
-var apiDomainDefault = 'api.segment.io,cdn.dreamdata.cloud,track.attributionapp.com,eu1.segmentapis.com,eu2.segmentapis.com,in.eu1.segmentapis.com,in.eu2.segmentapis.com,events.eu1.segmentapis.com,events.eu2.segmentapis.com';
+var apiDomainDefault =
+  "api.segment.io,cdn.dreamdata.cloud,track.attributionapp.com,seg-api.usebounce.com,seg-api.bounce.com";
 var apiDomain = apiDomainDefault;
 
-chrome.storage.local.get(['segment_api_domain'],(result) => {
-	apiDomain = result.segment_api_domain || apiDomainDefault;
-})
+chrome.storage.local.get(["segment_api_domain"], (result) => {
+  apiDomain = result.segment_api_domain || apiDomainDefault;
+});
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
-	if(namespace === 'local' && changes && changes.segment_api_domain) {
-		apiDomain = changes.segment_api_domain.newValue || apiDomainDefault;
-	}
+  if (namespace === "local" && changes && changes.segment_api_domain) {
+    apiDomain = changes.segment_api_domain.newValue || apiDomainDefault;
+  }
 });
 
 function zeroPad(i) {
-	if (i < 10) {
-		i = "0" + i
-	}
-	return i;
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
 
 function formatDateToTime(date) {
-	return date.toLocaleTimeString()
+  return date.toLocaleTimeString();
 }
 
 function withOpenTab(callback) {
-	chrome.tabs.query({
-		active: true,
-		currentWindow: true
-	}, (tabs) => {
-		var tab = tabs[0];
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      var tab = tabs[0];
 
-		if (tab) {
-			callback(tab);
-		}
-	});
+      if (tab) {
+        callback(tab);
+      }
+    }
+  );
 }
 
 function addEvent(event) {
-	trackedEvents.unshift(event);
-	chrome.runtime.sendMessage({ type: "new_event" });
+  trackedEvents.unshift(event);
+  chrome.runtime.sendMessage({ type: "new_event" });
 }
 
-function updateTrackedEventsForTab(tabId,connection) {
-	var sendEvents = [];
+function updateTrackedEventsForTab(tabId, connection) {
+  var sendEvents = [];
 
-	for(var i=0;i<trackedEvents.length;i++) {
-		if (trackedEvents[i].tabId == tabId) {
-			sendEvents.push(trackedEvents[i]);
-		}
-	}
+  for (var i = 0; i < trackedEvents.length; i++) {
+    if (trackedEvents[i].tabId == tabId) {
+      sendEvents.push(trackedEvents[i]);
+    }
+  }
 
-	connection.postMessage({
-		type: 'update',
-		events: sendEvents
-	});
+  connection.postMessage({
+    type: "update",
+    events: sendEvents,
+  });
 }
 
-function clearTrackedEventsForTab(tabId,port) {
-	var newTrackedEvents = [];
-	for(var i=0;i<trackedEvents.length;i++) {
-		if (trackedEvents[i].tabId != tabId) {
-			newTrackedEvents.push(trackedEvents[i]);
-		}
-	}
-	trackedEvents = newTrackedEvents;
+function clearTrackedEventsForTab(tabId, port) {
+  var newTrackedEvents = [];
+  for (var i = 0; i < trackedEvents.length; i++) {
+    if (trackedEvents[i].tabId != tabId) {
+      newTrackedEvents.push(trackedEvents[i]);
+    }
+  }
+  trackedEvents = newTrackedEvents;
 }
 
 chrome.runtime.onConnect.addListener((connection) => {
-	var connectionHandler = (msg) => {
-		var tabId = msg.tabId;
-		if (msg.type == 'update') {
-			updateTrackedEventsForTab(tabId, connection);
-		}
-		else if (msg.type == 'clear') {
-			clearTrackedEventsForTab(tabId, connection);
-			updateTrackedEventsForTab(tabId, connection);
-		}
-	};
-	connection.onMessage.addListener(connectionHandler);
+  var connectionHandler = (msg) => {
+    var tabId = msg.tabId;
+    if (msg.type == "update") {
+      updateTrackedEventsForTab(tabId, connection);
+    } else if (msg.type == "clear") {
+      clearTrackedEventsForTab(tabId, connection);
+      updateTrackedEventsForTab(tabId, connection);
+    }
+  };
+  connection.onMessage.addListener(connectionHandler);
 });
 
 function isSegmentApiCall(url) {
-	var apiDomainParts = apiDomain.split(',');
-	return apiDomainParts.findIndex(d => url.startsWith(`https://${d.trim()}`)) != -1;
+  var apiDomainParts = apiDomain.split(",");
+  return (
+    apiDomainParts.findIndex((d) => url.startsWith(`https://${d.trim()}`)) != -1
+  );
 }
 
 function onOwnServerResponse(url, callback) {
-	withOpenTab((tab) => {
-		try {
-			if ((new URL(tab.url)).host === (new URL(url)).host) {
-				callback();
-			}
-		}
-		catch(exception) {
-			console.log('Could not create URL.');
-			console.log(exception);
-		}
-	})
+  withOpenTab((tab) => {
+    try {
+      if (new URL(tab.url).host === new URL(url).host) {
+        callback();
+      }
+    } catch (exception) {
+      console.log("Could not create URL.");
+      console.log(exception);
+    }
+  });
 }
 
 function eventTypeToName(eventType) {
-	switch(eventType) {
-		case 'identify':
-			return 'Identify'
-		case 'pageLoad':
-			return 'Page Loaded'
-		case 'batch':
-			return 'Batch'
-	}
+  switch (eventType) {
+    case "identify":
+      return "Identify";
+    case "pageLoad":
+      return "Page Loaded";
+    case "batch":
+      return "Batch";
+  }
 }
 
 const onBeforeRequestHandler = (details) => {
-	if (isSegmentApiCall(details.url)) {
-		var bytes = new Uint8Array(details.requestBody.raw[0].bytes);
-		var decoder = new TextDecoder('utf-8');
-		var postedString = decoder.decode(bytes);
+  if (isSegmentApiCall(details.url)) {
+    var bytes = new Uint8Array(details.requestBody.raw[0].bytes);
+    var decoder = new TextDecoder("utf-8");
+    var postedString = decoder.decode(bytes);
 
-		var rawEvent = JSON.parse(postedString);
+    var rawEvent = JSON.parse(postedString);
 
-		var event = {
-			raw: postedString,
-			trackedTime: formatDateToTime(new Date()),
-		};
+    var event = {
+      raw: postedString,
+      trackedTime: formatDateToTime(new Date()),
+    };
 
-		withOpenTab((tab) => {
-			event.hostName = tab.url;
-			event.tabId = tab.id;
+    withOpenTab((tab) => {
+      event.hostName = tab.url;
+      event.tabId = tab.id;
 
-			if (
-				details.url.endsWith('/v1/t') ||
-				details.url.endsWith('/v2/t') ||
-				details.url.endsWith('/v1/track')
-			) {
-				event.type = 'track';
-			}
-			else if (
-				details.url.endsWith('/v1/i') ||
-				details.url.endsWith('/v2/i') ||
-				details.url.endsWith('/v1/identify')
-			) {
-				event.type = 'identify';
-			}
-			else if (
-				details.url.endsWith('/v1/p') ||
-				details.url.endsWith('/v2/p') ||
-				details.url.endsWith('/v1/page')
-			) {
-				event.type = 'pageLoad';
-			}
-			else if (
-				details.url.endsWith('/v1/batch') ||
-				details.url.endsWith('/v2/batch') ||
-				details.url.endsWith('/v1/b') ||
-				details.url.endsWith('/v2/b')
-			) {
-				event.type = 'batch';
-			}
+      if (
+        details.url.endsWith("/v1/t") ||
+        details.url.endsWith("/v2/t") ||
+        details.url.endsWith("/v1/track")
+      ) {
+        event.type = "track";
+      } else if (
+        details.url.endsWith("/v1/i") ||
+        details.url.endsWith("/v2/i") ||
+        details.url.endsWith("/v1/identify")
+      ) {
+        event.type = "identify";
+      } else if (
+        details.url.endsWith("/v1/p") ||
+        details.url.endsWith("/v2/p") ||
+        details.url.endsWith("/v1/page")
+      ) {
+        event.type = "pageLoad";
+      } else if (
+        details.url.endsWith("/v1/batch") ||
+        details.url.endsWith("/v2/batch") ||
+        details.url.endsWith("/v1/b") ||
+        details.url.endsWith("/v2/b")
+      ) {
+        event.type = "batch";
+      }
 
-			if (event.type) {
-				event.eventName = eventTypeToName(event.type) || rawEvent.event
-				addEvent(event);
-			}
-		});
-	}
+      if (event.type) {
+        event.eventName = eventTypeToName(event.type) || rawEvent.event;
+        addEvent(event);
+      }
+    });
+  }
 };
 
 chrome.webRequest.onBeforeRequest.addListener(
-	(details) => {
-		onBeforeRequestHandler(details);
-	},
-	{
-		urls: ['<all_urls>'],
-	},
-	["requestBody"]
+  (details) => {
+    onBeforeRequestHandler(details);
+  },
+  {
+    urls: ["<all_urls>"],
+  },
+  ["requestBody"]
 );
 
-
 const onHeadersReceivedHandler = (details) => {
-	onOwnServerResponse(details.url, () => {
-		const eventsHeader = details.responseHeaders.find(({ name }) => !!name && name.toLowerCase() === 'x-tracked-events');
-		if (!eventsHeader) return
+  onOwnServerResponse(details.url, () => {
+    const eventsHeader = details.responseHeaders.find(
+      ({ name }) => !!name && name.toLowerCase() === "x-tracked-events"
+    );
+    if (!eventsHeader) return;
 
-		withOpenTab((tab) => {
-			const serverTrackedEvents = JSON.parse(eventsHeader.value);
-			serverTrackedEvents.forEach((serverEvent) => {
-				const event = {
-					type: serverEvent.type,
-					eventName: serverEvent.event || eventTypeToName(serverEvent.type),
-					raw: JSON.stringify(serverEvent),
-					trackedTime: formatDateToTime(new Date(serverEvent.timestamp)),
-					hostName: details.url,
-					tabId: tab.id
-				};
-				addEvent(event);
-			})
-		});
-	})
+    withOpenTab((tab) => {
+      const serverTrackedEvents = JSON.parse(eventsHeader.value);
+      serverTrackedEvents.forEach((serverEvent) => {
+        const event = {
+          type: serverEvent.type,
+          eventName: serverEvent.event || eventTypeToName(serverEvent.type),
+          raw: JSON.stringify(serverEvent),
+          trackedTime: formatDateToTime(new Date(serverEvent.timestamp)),
+          hostName: details.url,
+          tabId: tab.id,
+        };
+        addEvent(event);
+      });
+    });
+  });
 };
 
 chrome.webRequest.onHeadersReceived.addListener(
-	(details) => {
-		onHeadersReceivedHandler(details);
-	},
-	{
-		urls: ['<all_urls>'],
-	},
-	['responseHeaders']
+  (details) => {
+    onHeadersReceivedHandler(details);
+  },
+  {
+    urls: ["<all_urls>"],
+  },
+  ["responseHeaders"]
 );
--- popup.js
+++ popup.js
@@ -1,165 +1,189 @@
-let apiDomainDefault = 'api.segment.io,cdn.dreamdata.cloud,track.attributionapp.com';
-//Obtained from: https://uxwing.com/copy-icon/
-const copyJsonSVG = '<svg width="32" height="32" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 115.77 122.88" style="enable-background:new 0 0 115.77 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02v0.02 v73.27v0.01h-0.02c-0.01,3.84-1.57,7.33-4.1,9.86c-2.51,2.5-5.98,4.06-9.82,4.07v0.02h-0.02h-61.7H40.1v-0.02 c-3.84-0.01-7.34-1.57-9.86-4.1c-2.5-2.51-4.06-5.98-4.07-9.82h-0.02v-0.02V92.51H13.96h-0.01v-0.02c-3.84-0.01-7.34-1.57-9.86-4.1 c-2.5-2.51-4.06-5.98-4.07-9.82H0v-0.02V13.96v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07V0h0.02h61.7 h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02V13.96L89.62,13.96z M79.04,21.69v-7.73v-0.02h0.02 c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v64.59v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h12.19V35.65 v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07v-0.02h0.02H79.04L79.04,21.69z M105.18,108.92V35.65v-0.02 h0.02c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v73.27v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h61.7h0.02 v0.02c0.91,0,1.75-0.39,2.37-1.01c0.61-0.61,1-1.46,1-2.37h-0.02V108.92L105.18,108.92z" stroke="gray" fill="white" fill-opacity="0.5"/></g></svg>'
+let apiDomainDefault =
+  "api.segment.io,cdn.dreamdata.cloud,track.attributionapp.com,seg-api.usebounce.com,seg-api.bounce.com";
 
 let connection = chrome.runtime.connect();
 
 function showEvent(number) {
-	document.getElementById('eventContent_' + number).style.display = 'block';
+  document.getElementById("eventContent_" + number).style.display = "block";
 }
-function printVariable(jsonObject,level) {
-	var returnString = '';
-	for (var key in jsonObject) {
-		if (jsonObject.hasOwnProperty(key)) {
-			returnString += '<div style="padding-left: ' + (level * 10) + 'px;">';
-			returnString += '<span class="key">';
-			returnString += key;
-			returnString += '</span>';
 
-			if (typeof jsonObject[key] == 'object') {
-				returnString += ' {' + printVariable(jsonObject[key],level + 1) + '}';
-			}
-			else {
-				var type = 'number';
-				if (isNaN(jsonObject[key])) {
-					type = 'string';
-				}
+function printVariable(jsonObject, level) {
+  let returnString = "";
+  for (let key in jsonObject) {
+    if (jsonObject.hasOwnProperty(key)) {
+      returnString += `<div style="padding-left: ${level * 10}px;">`;
+      returnString += `<span class="key">${key}</span>`;
 
-				returnString += ': ';
-				returnString += '<span class="' + type + '">';
-				returnString += jsonObject[key];
-				returnString += '</span>';
-			}
-			returnString += '</div>';
-		}
-	}
-	return returnString;
+      if (typeof jsonObject[key] === "object") {
+        returnString += ` {${printVariable(jsonObject[key], level + 1)}}`;
+      } else {
+        let type = isNaN(jsonObject[key]) ? "string" : "number";
+        returnString += `: <span class="${type}">${jsonObject[key]}</span>`;
+      }
+      returnString += `</div>`;
+    }
+  }
+  return returnString;
 }
+
 function queryForUpdate() {
-	chrome.tabs.query({ active: true, currentWindow: true },(tabs) => {
-		var currentTab = tabs[0];
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    var currentTab = tabs[0];
 
-		connection.postMessage({
-			type: "update",
-			tabId: currentTab.id
-		});
-	});
+    connection.postMessage({
+      type: "update",
+      tabId: currentTab.id,
+    });
+  });
 }
 
 function clearTabLog() {
-	chrome.tabs.query({ active: true, currentWindow: true },(tabs) => {
-		var currentTab = tabs[0];
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    var currentTab = tabs[0];
 
-		connection.postMessage({
-			type: "clear",
-			tabId: currentTab.id
-		});
-	});
+    connection.postMessage({
+      type: "clear",
+      tabId: currentTab.id,
+    });
+  });
 }
 
 queryForUpdate();
 
 chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
-	if (message.type == 'new_event') {
-		queryForUpdate();
-	}
+  if (message.type == "new_event") {
+    queryForUpdate();
+  }
 });
 
 connection.onMessage.addListener((msg) => {
-	if (msg.type == "update") {
-		var prettyEventsString = '';
+  if (msg.type == "update") {
+    var prettyEventsString = "";
+    let anonymousId = "";
+    let userId = "";
 
-		if (msg.events.length > 0) {
-			for (var i=0;i<msg.events.length;i++) {
-				var event = msg.events[i];
+    if (msg.events.length > 0) {
+      for (var i = 0; i < msg.events.length; i++) {
+        var event = msg.events[i];
 
-				var jsonObject = JSON.parse(event.raw)
-				var eventString = '';
+        var jsonObject = JSON.parse(event.raw);
+        var eventString = "";
+        const url = new URL(event.hostName);
+        const eventName = event.eventName;
+        const pathname = url.pathname;
+        const trackedTime = event.trackedTime;
+        const eventType = event.type;
+        const isExposure = eventName === "$exposure";
 
-				eventString += '<div class="eventTracked eventType_' + event.type + '">';
-					eventString += '<div class="eventInfo" id="eventInfo_'+i+'"><span class="eventName">' 
-						eventString += '<div class="copyEvent" id="copy_'+i+'"><a title="Copy json to clipboard">'+ copyJsonSVG + '</a></div>'
-							+ event.eventName + '</span> - ' + event.trackedTime + '<br />' + event.hostName 
-						eventString += '<div class="eventContent" id="eventContent_' + i + '" >';
-							eventString += printVariable(jsonObject,0);
-						eventString += '</div>';
-					eventString += '</div>';
-				eventString += '</div>';
+        let extra = "";
+        if (isExposure) {
+          const props = jsonObject.properties;
+          const flagKey = props?.flag_key;
+          const variant = props?.variant;
 
-				prettyEventsString += eventString;
-			}
-		}
-		else {
-			prettyEventsString += 'No events tracked in this tab yet.';
-		}
-		document.getElementById('trackMessages').innerHTML = prettyEventsString;
+          extra = `<div class="eventHeader_extra"><span class="flagKey">${flagKey}</span> - ${variant}</div>`;
+        }
 
-		for (var i=0;i<msg.events.length;i++) {
-			const number = i;
-			document.getElementById('eventInfo_' + number).onclick = function() {
-				if (document.getElementById('eventContent_' + number).style.display == 'block') {
-					document.getElementById('eventContent_' + number).style.display = 'none';
-				}
-				else {
-					document.getElementById('eventContent_' + number).style.display = 'block';
-				}
-			};
-			document.getElementById('copy_'+number).onclick = (event) => {
-				navigator.clipboard.writeText(msg.events[number].raw);
-				event.stopPropagation();
-			}
-			
-		}
+        const isNewAnonymousId = jsonObject.anonymousId !== anonymousId;
+        const isNewUserId = jsonObject.userId !== userId;
+        if (isNewAnonymousId || isNewUserId) {
+          anonymousId = jsonObject.anonymousId;
+          userId = jsonObject.userId;
 
-		
-	}
+          extra += `<div class="eventHeader_extra"><span class="newAnonymousId"><span class="newValue">anonymousId</span>: ${anonymousId}</span></div>`;
+          extra += `<div class="eventHeader_extra"><span class="newUserId"><span class="newValue">userId</span>: ${userId}</span></div>`;
+        }
+
+        eventString += `<div class="eventTracked eventType_${eventType}">
+					<div class="eventInfo">
+					  <div class="eventHeader" id="eventInfo_${i}">
+								<div class="eventHeader_title"><span class="eventName">${eventName}</span> - ${pathname} - ${trackedTime}</div>
+                ${extra}
+						</div>
+						<div class="eventContent" id="eventContent_${i}">
+							${printVariable(jsonObject, 0)}
+						</div>
+					</div>
+				</div>`;
+
+        prettyEventsString += eventString;
+      }
+    } else {
+      prettyEventsString += "No events tracked in this tab yet.";
+    }
+    document.getElementById("trackMessages").innerHTML = prettyEventsString;
+
+    for (var i = 0; i < msg.events.length; i++) {
+      const number = i;
+      document.getElementById("eventInfo_" + number).onclick = function () {
+        if (
+          document.getElementById("eventContent_" + number).style.display ==
+          "flex"
+        ) {
+          document.getElementById("eventContent_" + number).style.display =
+            "none";
+        } else {
+          document.getElementById("eventContent_" + number).style.display =
+            "flex";
+        }
+      };
+    }
+  }
 });
 
 function filterEvents(keyPressedEvent) {
-	var filter = new RegExp(keyPressedEvent.target.value, 'gi');
-	var eventElements = document.getElementById('trackMessages').getElementsByClassName('eventTracked');
-	for(eventElement of eventElements) {
-		var eventName = eventElement.getElementsByClassName('eventName')[0].textContent;
-		if (eventName.match(filter)) {
-			eventElement.classList.remove('hidden');
-		} else {
-			eventElement.classList.add('hidden');
-		}
-	}
+  var filter = new RegExp(keyPressedEvent.target.value, "gi");
+  var eventElements = document
+    .getElementById("trackMessages")
+    .getElementsByClassName("eventTracked");
+  for (eventElement of eventElements) {
+    let eventName =
+      eventElement.getElementsByClassName("eventName")[0].textContent;
+    let flagKey =
+      eventElement.getElementsByClassName("flagKey")[0]?.textContent;
+
+    if (eventName.match(filter) || (!!flagKey && flagKey.match(filter))) {
+      eventElement.classList.remove("hidden");
+    } else {
+      eventElement.classList.add("hidden");
+    }
+  }
 }
 
 function toggleConfiguration() {
-	var configurationDiv = document.getElementById('configurationDiv');
-	configurationDiv.hidden = !configurationDiv.hidden;
+  var configurationDiv = document.getElementById("configurationDiv");
+  configurationDiv.hidden = !configurationDiv.hidden;
 
-	var contentDiv = document.getElementById('contentDiv');
-	contentDiv.hidden = !contentDiv.hidden;
+  var contentDiv = document.getElementById("contentDiv");
+  contentDiv.hidden = !contentDiv.hidden;
 }
 
 function updateApiDomain(apiDomain) {
-	chrome.storage.local.set({segment_api_domain: apiDomain || apiDomainDefault}, function() {
-	});
+  chrome.storage.local.set(
+    { segment_api_domain: apiDomain || apiDomainDefault },
+    function () {}
+  );
 }
 
 function handleApiDomainUpdates() {
-	var apiDomainInput = document.getElementById('apiDomain');
+  var apiDomainInput = document.getElementById("apiDomain");
 
-	chrome.storage.local.get(['segment_api_domain'], function(result) {
-		apiDomainInput.value = result.segment_api_domain || apiDomainDefault;
-		apiDomainInput.onchange = () => updateApiDomain(apiDomainInput.value);
-	});
+  chrome.storage.local.get(["segment_api_domain"], function (result) {
+    apiDomainInput.value = result.segment_api_domain || apiDomainDefault;
+    apiDomainInput.onchange = () => updateApiDomain(apiDomainInput.value);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	var clearButton = document.getElementById('clearButton');
-	clearButton.onclick = clearTabLog;
+document.addEventListener("DOMContentLoaded", function () {
+  var clearButton = document.getElementById("clearButton");
+  clearButton.onclick = clearTabLog;
 
-	var filterInput = document.getElementById('filterInput');
-	filterInput.onkeyup = filterEvents;
-	filterInput.focus();
+  var filterInput = document.getElementById("filterInput");
+  filterInput.onkeyup = filterEvents;
+  filterInput.focus();
 
-	var configureButton = document.getElementById('configureButton');
-	configureButton.onclick = toggleConfiguration;
+  var configureButton = document.getElementById("configureButton");
+  configureButton.onclick = toggleConfiguration;
 
-	handleApiDomainUpdates();
+  handleApiDomainUpdates();
 });
